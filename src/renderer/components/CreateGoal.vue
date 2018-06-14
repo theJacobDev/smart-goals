8744d5b6df438125ce7efd74b5333dfacc3bcab3
Jakub Aleksandrowicz <jacob@jacobdev.com>
2018-06-14 10:29:45 +0200

    improve create goal component

--- src/renderer/components/CreateGoal.vue
+++ src/renderer/components/CreateGoal.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-row type="flex" justify="center">
-      <page-header title="Add new goal" icon="edit-outline">
+      <page-header title="Add new goal" icon="edit">
         <template slot="description">
 
         </template>
@@ -9,43 +9,45 @@
     </el-row>
     <el-row type="flex" justify="center">
       <el-col :span="20">
-        <el-form :model="form" label-position="left" :rules="rules" :status-icon="true" ref="goalForm" label-width="7rem">
-          <el-form-item class="remove-required-icon" label="Specific" prop="specific">
-            <el-input placeholder="Name" v-model="form.specific"></el-input>
-          </el-form-item>
-          <el-form-item label="Measurable" prop="measurables">
-            <el-input placeholder="Measurable" ref="measurable" @keyup.enter.native="addMeasurable" v-model="newMeasurement">
-              <el-button @click="addMeasurable" slot="append" icon="el-icon-list">Add</el-button>
-            </el-input>
-            <p v-for="measurement in form.measurables" :key="measurement.id">
-              <i class="el-icon-circle-close-outline delete-icon" @click="removeItemFromList('measurables', measurement.id)" />
-              {{ measurement.name }}
-            </p>
-          </el-form-item>
-          <el-form-item label="Achievable" prop="achievables">
-            <el-input placeholder="Achievable" ref="measurable" @keyup.enter.native="addAchievable" v-model="newAchievable">
-              <el-button @click="addAchievable" slot="append" icon="el-icon-list">Add</el-button>
-            </el-input>
-            <p v-for="achievable in form.achievables" :key="achievable.id">
-              <i class="el-icon-circle-close-outline delete-icon" @click="removeItemFromList('achievables', achievable.id)" />
-              {{ achievable.name }}
-            </p>
-          </el-form-item>
-          <el-form-item class="remove-required-icon" label="Relevant" prop="relevant">
-            <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 6}" v-model="form.relevant"></el-input>
-          </el-form-item>
-          <el-form-item class="remove-required-icon" label="Time-bound" prop="timeBound">
-            <el-radio-group v-model="dateSelectionType" class="date-type">
-              <el-radio-button label="month"></el-radio-button>
-              <el-radio-button label="year"></el-radio-button>
-            </el-radio-group>
-            <el-date-picker :type="dateSelectionType" placeholder="Pick a date" v-model="form.timeBound"></el-date-picker>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="success" @click="createGoal" plain>Create</el-button>
-            <el-button type="danger" @click="resetForm" plain>Reset</el-button>
-          </el-form-item>
-        </el-form>
+        <el-card>
+          <el-form :model="form" label-position="left" :rules="rules" :status-icon="true" ref="goalForm" label-width="7rem">
+            <el-form-item class="remove-required-icon" label="Specific" prop="specific">
+              <el-input placeholder="Name" v-model="form.specific"></el-input>
+            </el-form-item>
+            <el-form-item label="Measurable" prop="measurables">
+              <el-input placeholder="Measurable" ref="measurable" @keyup.enter.native="addMeasurable" v-model="newMeasurement">
+                <el-button @click="addMeasurable" slot="append" icon="el-icon-list">Add</el-button>
+              </el-input>
+              <p v-for="measurement in form.measurables" :key="measurement.id">
+                <i class="el-icon-circle-close-outline delete-icon" @click="removeItemFromList('measurables', measurement.id)" />
+                {{ measurement.name }}
+              </p>
+            </el-form-item>
+            <el-form-item label="Achievable" prop="achievables">
+              <el-input placeholder="Achievable" ref="measurable" @keyup.enter.native="addAchievable" v-model="newAchievable">
+                <el-button @click="addAchievable" slot="append" icon="el-icon-list">Add</el-button>
+              </el-input>
+              <p v-for="achievable in form.achievables" :key="achievable.id">
+                <i class="el-icon-circle-close-outline delete-icon" @click="removeItemFromList('achievables', achievable.id)" />
+                {{ achievable.name }}
+              </p>
+            </el-form-item>
+            <el-form-item class="remove-required-icon" label="Relevant" prop="relevant">
+              <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 6}" v-model="form.relevant"></el-input>
+            </el-form-item>
+            <el-form-item class="remove-required-icon" label="Time-bound" prop="timeBound">
+              <el-radio-group v-model="dateSelectionType" class="date-type">
+                <el-radio-button label="month"></el-radio-button>
+                <el-radio-button label="year"></el-radio-button>
+              </el-radio-group>
+              <el-date-picker :type="dateSelectionType" placeholder="Pick a date" v-model="form.timeBound"></el-date-picker>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="success" @click="createGoal" plain>Create</el-button>
+              <el-button type="danger" @click="resetForm" plain>Reset</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
       </el-col>
     </el-row>
   </div>
@@ -151,6 +153,10 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/scss/palette.scss';
+
+.el-card {
+  padding: 20px;
+}
 
 .date-type {
   margin-bottom: 1rem;
